37c700e7acd3e3a6bda44004ab1190ad163bd960
WoolimLee <woolimlee.dev@gmail.com>
2024-03-07 16:24:31 +0900

    Tooltip 추가

--- Assets/Scenes/PlayerPrefsExample/PlayerPrefsExample2.cs
+++ Assets/Scenes/PlayerPrefsExample/PlayerPrefsExample2.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
@@ -13,6 +14,8 @@ public class PlayerPrefsExample2 : MonoBehaviour
     public GameObject AgeInput;
     public GameObject HightInput;
 
+    public GameObject Tooltip;
+
     private void Start()
     {
         if(playerData == null)
@@ -21,11 +24,25 @@ public class PlayerPrefsExample2 : MonoBehaviour
         }
     }
 
+    void DisableTooltip()
+    {
+        Tooltip.SetActive(false);
+    }
+
     public void SaveData()
     {
-        PlayerPrefs.SetString("PlayerName", NameInput.GetComponent<InputField>().text);
-        PlayerPrefs.SetInt("PlayerAge", Convert.ToInt32(AgeInput.GetComponent<InputField>().text));
-        PlayerPrefs.SetInt("PlayerHight", Convert.ToInt32(HightInput.GetComponent<InputField>().text));
+        if(NameInput.GetComponent<InputField>().text != "" && AgeInput.GetComponent<InputField>().text !=  "" && HightInput.GetComponent<InputField>().text != "")
+        {
+            PlayerPrefs.SetString("PlayerName", NameInput.GetComponent<InputField>().text);
+            PlayerPrefs.SetInt("PlayerAge", Convert.ToInt32(AgeInput.GetComponent<InputField>().text));
+            PlayerPrefs.SetInt("PlayerHight", Convert.ToInt32(HightInput.GetComponent<InputField>().text));
+        }
+        else
+        {
+            Tooltip.SetActive(true);
+            Tooltip.GetComponent<Text>().text = "누락된 내용이있어요..";
+            Invoke(nameof(DisableTooltip), 1);
+        }
     }
 
     //진짜지나자니자ㅣ자 가능하면 매니저로 관리하자..
